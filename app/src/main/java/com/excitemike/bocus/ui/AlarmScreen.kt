ea75f4097efc03a28e3dee153e945682253ded5c
Mike Meyer <mike@meyermike.com>
2026-07-25 03:26:13 -0400

    Colorful swipe

--- app/src/main/java/com/excitemike/bocus/ui/AlarmScreen.kt
+++ app/src/main/java/com/excitemike/bocus/ui/AlarmScreen.kt
@@ -1,28 +1,35 @@
 package com.excitemike.bocus.ui
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SwipeToDismissBox
+import androidx.compose.material3.SwipeToDismissBoxValue
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -101,13 +108,49 @@ fun AlarmList(
                 ) { alarm ->
                     val index = lazy { alarms.indexOfFirst { it.id == alarm.id } }
                     val deleteAlarmTemplate = stringResource(R.string.confirm_delete_alarm)
+                    val swipeToDismissState = rememberSwipeToDismissBoxState()
                     SwipeToDismissBox(
-                        state = rememberSwipeToDismissBoxState(),
+                        state = swipeToDismissState,
                         backgroundContent = {
-                            Box { Icon (
-                                imageVector = Icons.Default.Delete,
-                                contentDescription = null
-                            ) }
+                            val deleteColor = Color(1f,0f,0f,0.85f)
+                            val bgColor =  if (swipeToDismissState.targetValue == SwipeToDismissBoxValue.Settled) {
+                                Color.Transparent
+                            } else {
+                                deleteColor
+                            }
+
+                            Row (
+                                Modifier.padding(4.dp)
+                                    .background(
+                                        color = bgColor,
+                                        shape = RoundedCornerShape(16.dp),
+                                    ),
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                if (swipeToDismissState.targetValue != SwipeToDismissBoxValue.EndToStart) {
+                                    Icon (
+                                        modifier = Modifier
+                                            .weight(1f)
+                                            .fillMaxHeight(),
+                                        imageVector = Icons.Default.Delete,
+                                        contentDescription = null
+                                    )
+                                } else {
+                                    Spacer(Modifier.weight(1f))
+                                }
+                                Spacer(Modifier.weight(1f))
+                                if (swipeToDismissState.targetValue != SwipeToDismissBoxValue.StartToEnd) {
+                                    Icon (
+                                        modifier = Modifier
+                                            .weight(1f)
+                                            .fillMaxHeight(),
+                                        imageVector = Icons.Default.Delete,
+                                        contentDescription = null
+                                    )
+                                } else {
+                                    Spacer(Modifier.weight(1f))
+                                }
+                            }
                         },
                         onDismiss = {
                             val message = String.format(
