536a1053d31e3953f358bad8284570040b1c3ba5
Mike Meyer <mike@themeaningfullifecenter.com>
2026-08-07 14:52:54 -0400

    support button now wiggles

--- app/src/main/java/com/excitemike/bocus/ui/component/SupportButton.kt
+++ app/src/main/java/com/excitemike/bocus/ui/component/SupportButton.kt
@@ -1,10 +1,18 @@
 package com.excitemike.bocus.ui.component
 
 import android.app.Activity
+import androidx.compose.animation.core.Easing
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
@@ -12,6 +20,7 @@ import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -20,34 +29,67 @@ import androidx.compose.ui.unit.dp
 import com.excitemike.bocus.R
 import com.excitemike.bocus.ui.viewmodel.AppPurchasedState
 import com.excitemike.bocus.ui.viewmodel.BillingViewModel
+import kotlin.math.max
+
+private const val PADDING = 8f
+private const val ANIM_MAGNITUDE = 8f
 
 @Composable
 fun SupportButton(
     activity: Activity,
     billingViewModel: BillingViewModel,
+    onError: (Int) -> Unit,
 ) {
+    val infiniteTransition = rememberInfiniteTransition(label = "support_anim")
+    val animVal by infiniteTransition.animateFloat(
+        initialValue = -1f,
+        targetValue = 1f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(150, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+    val animMagnitude by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = ANIM_MAGNITUDE,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                3_000,
+                easing = Easing { max(0f, 6f * it - 5f) }
+            ),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+    val animOffset = (animMagnitude * animVal).dp
     val showSupportButton =
         rememberSaveable(billingViewModel.purchasedState) { billingViewModel.purchasedState.value != AppPurchasedState.PURCHASED }
     if (showSupportButton) {
         BocusButton(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(8.dp),
+                .padding(
+                    start = PADDING.dp + animOffset,
+                    end = PADDING.dp - animOffset
+                ),
             onClick = {
-                billingViewModel.beginPurchaseFlow(activity, BillingViewModel.PRODUCT_ID)
+                val err = billingViewModel.beginPurchaseFlow(activity, BillingViewModel.PRODUCT_ID)
+                if (err != null) {
+                    onError(err)
+                }
             },
         ) {
             Row(
                 horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically
+                verticalAlignment = Alignment.CenterVertically,
             ) {
                 val supportText = stringResource(R.string.support_this_app)
                 Icon(
-                    modifier = Modifier.size(32.dp),
+                    modifier = Modifier
+                        .height(32.dp),
                     imageVector = Icons.Default.Favorite,
                     contentDescription = supportText,
                 )
-                Spacer(Modifier.size(8.dp))
+                Spacer(modifier = Modifier.size(PADDING.dp))
                 Text(text = supportText)
             }
         }
